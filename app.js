1cf7945dd0816d0410bca4ff05013b7235ea1e9e
jeffreytoconnell <jeffreytoconnell@gmail.com>
2017-06-30 11:41:10 -0700

    added router post

--- app.js
+++ app.js
@@ -3,7 +3,27 @@ const app = express();
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const mongodb = require('mongodb');
+const router = express.Router();
 
 const Course = require('./models');
+const url = 'mongodb://localhost:8080';
 
 app.use(bodyParser.json());
+
+// GET HOME PAGE
+router.get('/', function(req, res, next) {
+    res.render('index');
+});
+
+// POST
+router.post('/course', function(req, res, next) {
+    var course = {
+        date: req.body.date,
+        course: req.body.course,
+        location: req.body.location,
+        courseDirector: req.body.courseDirector,
+        assist1: req.body.assist1,
+        assist2: req.body.assist2,
+        assist3: req.body.assist3
+    }
+});
