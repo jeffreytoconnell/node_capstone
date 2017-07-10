4f2598ced848aaa243f157bfaf331189df9e0a4d
jeffreytoconnell <jeffreytoconnell@gmail.com>
2017-07-09 18:21:16 -0700

    authen

--- app.js
+++ app.js
@@ -3,15 +3,105 @@ const app = express();
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const mongodb = require('mongodb');
-
-
+const {BasicStrategy} = require('passport-http');
+const passport = require('passport');
+const {User} = require('./models');
 // const Course = require('./models/course');
 const url = 'mongodb://jeff:gunner@ds163181.mlab.com:63181/thinkful';
 mongoose.connect(url);
+const basicStrategy = new BasicStrategy((username, password, callback) => {
+  let user;
+  User
+    .findOne({username: username})
+    .exec()
+    .then(_user => {
+      user = _user;
+      if (!user) {
+        return callback(null, false);
+      }
+      return user.validatePassword(password);
+    })
+    .then(isValid => {
+      if (!isValid) {
+        return callback(null, false);
+      }
+      else {
+        return callback(null, user);
+      }
+    })
+    .catch(err => callback(err));
+});
+
+passport.use(basicStrategy);
+
+router.post('/', (req, res) => {
+  if (!req.body) {
+    return res.status(400).json({message: 'No request body'});
+  }
+
+  if (!('username' in req.body)) {
+    return res.status(422).json({message: 'Missing field: username'});
+  }
+
+  let {username, password, firstName, lastName} = req.body;
+
+  if (typeof username !== 'string') {
+    return res.status(422).json({message: 'Incorrect field type: username'});
+  }
+
+  username = username.trim();
+
+  if (username === '') {
+    return res.status(422).json({message: 'Incorrect field length: username'});
+  }
+
+  if (!(password)) {
+    return res.status(422).json({message: 'Missing field: password'});
+  }
+
+  if (typeof password !== 'string') {
+    return res.status(422).json({message: 'Incorrect field type: password'});
+  }
+
+  password = password.trim();
+
+  if (password === '') {
+    return res.status(422).json({message: 'Incorrect field length: password'});
+  }
+
+  // check for existing user
+  return User
+    .find({username})
+    .count()
+    .exec()
+    .then(count => {
+      if (count > 0) {
+        return res.status(422).json({message: 'username already taken'});
+      }
+      // if no existing user, hash password
+      return User.hashPassword(password)
+    })
+    .then(hash => {
+      return User
+        .create({
+          username: username,
+          password: hash,
+          firstName: firstName,
+          lastName: lastName
+        })
+    })
+    .then(user => {
+      return res.status(201).json(user.apiRepr());
+    })
+    .catch(err => {
+      res.status(500).json({message: 'Internal server error'})
+    });
+});
+
 
 
 app.use(bodyParser.json());
-app.use(express.static('public'))
+app.use(express.static('public')) // USE PUBLIC DIRECTORY
 
 
 // Course Schema
@@ -50,7 +140,7 @@ var Course = mongoose.model('course', courseSchema)
 
 // GET HOME PAGE
 app.get('/', function(req, res, next) {
-    res.render('index');
+    res.render('index.html');
 });
 
 // POST
